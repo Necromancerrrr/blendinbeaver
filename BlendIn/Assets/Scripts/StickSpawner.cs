1d34f5a3f2be1d403c628d04bf156f4ac1f6c8db
Jamie Robinson <jamiegribblerobinson@gmail.com>
2026-05-21 09:46:35 +1000

    sticks dont fall through floor & spawn around trees

--- BlendIn/Assets/Scripts/StickSpawner.cs
+++ BlendIn/Assets/Scripts/StickSpawner.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class StickSpawner : MonoBehaviour
 {
-    [SerializeField] private float spawnRate = 1f;
+    [SerializeField] private float spawnRatePerSecond = 1f;
 
-    [SerializeField] private int bounds = 30;
+    [SerializeField] private int spawnBoundsAroundTree = 5;
 
     [SerializeField] private GameObject[] sticks;
 
-    [SerializeField] private int maxSticks = 20;
+    [SerializeField] private Transform[] spawnPoints;
+
+    [SerializeField] private int maxSticks = 10;
     public int numberOfSticks;
 
     private float timer = 0;
@@ -22,11 +24,11 @@ public class StickSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (timer >= spawnRate) // every x seconds a stick will spawn
+        if (timer >= spawnRatePerSecond) // every x seconds a stick will spawn
         {
             if (numberOfSticks != maxSticks) // if we aren't at max sticks spawn more
             {
-                Instantiate(RandomStick(), RandomPosition(), Quaternion.identity);
+                Instantiate(RandomStick(), RandomSpawnPoint(), RandomAngle());
                 numberOfSticks += 1;
             }
             timer = 0;
@@ -40,12 +42,28 @@ public class StickSpawner : MonoBehaviour
         return sticks[Random.Range(0, sticks.Length)];
     }
 
+    private Vector3 RandomSpawnPoint()
+    {
+        Transform randPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        float randXPos = Random.Range(-spawnBoundsAroundTree, spawnBoundsAroundTree);
+        float randZPos = Random.Range(-spawnBoundsAroundTree, spawnBoundsAroundTree);
+
+        return new Vector3(randPoint.position.x + randXPos, 20, randPoint.position.z + randZPos);
+    }
+
+    private Quaternion RandomAngle()
+    {
+        return Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f));
+    }
+
+    /*
     private Vector3 RandomPosition() // gets a random pos based on the bounds float
     {
         float xPos = Random.Range(-bounds, bounds);
         float zPos = Random.Range(-bounds, bounds);
 
-        return new Vector3(xPos, 10, zPos);
+        return new Vector3(xPos, 20, zPos);
     }
-
+    */
 }
